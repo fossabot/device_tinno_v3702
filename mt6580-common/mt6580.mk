060a11ffe222abcfdd28f9f6f0f5ba2b686700b7
aleha-druga <aleha.druga@gmail.com>
2017-09-27 16:54:08 +0500

    upgrade

--- mt6580-common/mt6580.mk
+++ mt6580-common/mt6580.mk
@@ -57,24 +57,28 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/multi_init.rc:root/multi_init.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt6580.rc:root/ueventd.mt6580.rc
 
-# #RIL Hack
-# PRODUCT_COPY_FILES += \
-#     $(LOCAL_PATH)/mtk_ril/bin/ccci_fsd:system/bin/ccci_fsd \
-#     $(LOCAL_PATH)/mtk_ril/bin/ccci_mdinit:system/bin/ccci_mdinit \
-#     $(LOCAL_PATH)/mtk_ril/bin/gsm0710muxd:system/bin/gsm0710muxd \
-#     $(LOCAL_PATH)/mtk_ril/bin/gsm0710muxdmd2:system/bin/gsm0710muxdmd2 \
-#     $(LOCAL_PATH)/mtk_ril/bin/md_ctrl:system/bin/md_ctrl \
-#     $(LOCAL_PATH)/mtk_ril/bin/mdlogger:system/bin/mdlogger \
-#     $(LOCAL_PATH)/mtk_ril/bin/mtkrild:system/bin/mtkrild \
-#     $(LOCAL_PATH)/mtk_ril/bin/mtkrildmd2:system/bin/mtkrildmd2 \
-#     $(LOCAL_PATH)/mtk_ril/bin/muxreport:system/bin/muxreport \
-#     $(LOCAL_PATH)/mtk_ril/bin/terservice:system/bin/terservice \
-#     $(LOCAL_PATH)/mtk_ril/lib/libccci_util.so:system/lib/libccci_util.so \
-#     $(LOCAL_PATH)/mtk_ril/lib/libmal.so:system/lib/libmal.so \
-#     $(LOCAL_PATH)/mtk_ril/lib/librilmtk.so:system/lib/librilmtk.so \
-#     $(LOCAL_PATH)/mtk_ril/lib/librilmtkmd2.so:system/lib/librilmtkmd2.so \
-#     $(LOCAL_PATH)/mtk_ril/lib/mtk-ril.so:system/lib/mtk-ril.so \
-#     $(LOCAL_PATH)/mtk_ril/lib/mtk-rilmd2.so:system/lib/mtk-rilmd2.so
+#RIL Hack
+RIL_HACK := yes
+
+ifeq ($(RIL_HACK),yes)
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/mtk_ril/bin/ccci_fsd:system/bin/ccci_fsd \
+     $(LOCAL_PATH)/mtk_ril/bin/ccci_mdinit:system/bin/ccci_mdinit \
+     $(LOCAL_PATH)/mtk_ril/bin/gsm0710muxd:system/bin/gsm0710muxd \
+     $(LOCAL_PATH)/mtk_ril/bin/gsm0710muxdmd2:system/bin/gsm0710muxdmd2 \
+     $(LOCAL_PATH)/mtk_ril/bin/md_ctrl:system/bin/md_ctrl \
+     $(LOCAL_PATH)/mtk_ril/bin/mdlogger:system/bin/mdlogger \
+     $(LOCAL_PATH)/mtk_ril/bin/mtkrild:system/bin/mtkrild \
+     $(LOCAL_PATH)/mtk_ril/bin/mtkrildmd2:system/bin/mtkrildmd2 \
+     $(LOCAL_PATH)/mtk_ril/bin/muxreport:system/bin/muxreport \
+     $(LOCAL_PATH)/mtk_ril/bin/terservice:system/bin/terservice \
+     $(LOCAL_PATH)/mtk_ril/lib/libccci_util.so:system/lib/libccci_util.so \
+     $(LOCAL_PATH)/mtk_ril/lib/libmal.so:system/lib/libmal.so \
+     $(LOCAL_PATH)/mtk_ril/lib/librilmtk.so:system/lib/librilmtk.so \
+     $(LOCAL_PATH)/mtk_ril/lib/librilmtkmd2.so:system/lib/librilmtkmd2.so \
+     $(LOCAL_PATH)/mtk_ril/lib/mtk-ril.so:system/lib/mtk-ril.so \
+     $(LOCAL_PATH)/mtk_ril/lib/mtk-rilmd2.so:system/lib/mtk-rilmd2.so
+endif
 
 # Audio
 PRODUCT_PACKAGES += \
