7ae70be94fa96a29cda3c636f6c3f346762a18eb
aleha-druga <aleha.druga@gmail.com>
2017-09-28 22:06:57 +0500

    modified mk

--- vendor/v3702-vendor-blobs.mk
+++ vendor/v3702-vendor-blobs.mk
@@ -1,9 +1,7 @@
 VENDOR_FILES := device/tinno/v3702/vendor
     
 PRODUCT_COPY_FILES := \
-    $(call find-copy-subdir-files,*,$(VENDOR_FILES)/bin,system/bin) \
     $(call find-copy-subdir-files,*,$(VENDOR_FILES)/lib,system/lib) \
-    $(call find-copy-subdir-files,*,$(VENDOR_FILES)/etc,system/etc) \
     $(call find-copy-subdir-files,*,$(VENDOR_FILES)/vendor/bin,system/vendor/bin) \
     $(call find-copy-subdir-files,*,$(VENDOR_FILES)/vendor/etc,system/vendor/etc) \
     $(call find-copy-subdir-files,*,$(VENDOR_FILES)/vendor/firmware,system/vendor/firmware) \
